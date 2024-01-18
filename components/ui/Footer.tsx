04e8c0c96afad35d86b08c631279f643a30b2429
Mehmet Erim Gökhan <mehmeterimgokhan99@gmail.com>
2024-01-18 17:08:25 +0300

    text-fix-opensrc

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <a href="github.com/megerim/wordgame" className='text-blue-500'>GitHub</a>
+        <a href="github.com/megerim/wordgame" className='text-blue-500'>GitHub - Açık Kaynak</a>
     </div>
   )
 }
